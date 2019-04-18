8a1fc3ec3fe88d2cd0a3a36869346d0cd30d916d
baconsoi9999 <baconsoi9999@gmail.com>
2019-04-18 14:58:49 +0700

    update dmkho

---
+++ app/src/main/java/com/example/quanlykho/dmkho/DMKhoActivity.java
@@ -0,0 +1,13 @@
+package com.example.quanlykho.dmkho;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DMKhoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dmkho);
+    }
+}
